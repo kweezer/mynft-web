80d51f812ae3ffb4010d4a1fa65ad0b6de9085b0
kweezer <kweezerkwee@gmail.com>
2021-11-20 21:38:32 -0800

    Add more detail to manifest and feedback link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
-import AddressForm from './components/AddressForm.js'
+import "./App.css";
+import AddressForm from "./components/AddressForm.js";
 
 function App() {
   return (
     <div className="App">
-      <AddressForm/>
+      <AddressForm />
+      <small className="feedback">
+        Share your feedback&nbsp;
+        <a href="https://twitter.com/ikweezer" target="_blank" rel="noreferrer">
+          @ikweezer
+        </a>
+      </small>
     </div>
   );
 }
