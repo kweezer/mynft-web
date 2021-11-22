b671979b88797945038e33ca0b0512df58359418
kweezer <kweezerkwee@gmail.com>
2021-11-21 16:14:34 -0800

    Add contract name group

--- src/components/NFT.js
+++ src/components/NFT.js
@@ -1,24 +1,19 @@
 import React from "react";
 
 class NFT extends React.Component {
-
   render() {
     const nft = this.props.data;
     if (nft === undefined) {
-      return <div></div>
-    }
-    let name = nft.name;
-    if (nft.name === null || nft.name === "" || nft.name === undefined) {
-      name = this.props.contractName;
+      return <div></div>;
     }
     const imageUrl = nft.imageUrl;
     const permaLink = nft.permaLink;
 
     return (
       <div className="nft">
-        <h5>{name}</h5>
+        <h5>{nft.name}</h5>
         <a href={permaLink} target="_blank" rel="noreferrer">
-          <img src={imageUrl} alt={name} />
+          <img src={imageUrl} alt={nft.name} />
         </a>
       </div>
     );
--- src/components/NFTCollection.js
+++ src/components/NFTCollection.js
@@ -2,19 +2,30 @@ import React from "react";
 import NFT from "./NFT.js";
 
 class NFTCollection extends React.Component {
-
   render() {
     const nftCollections = this.props.data;
     if (nftCollections === undefined) {
-      return <div></div>
+      return <div></div>;
     }
     return (
       <div className="nft-collection">
         {nftCollections.map((nftData, i) => (
           <div>
-            {nftData.nfts.map((nft, i) => (
-              <NFT data={nft} contractName={nftData.name} key={i} />
-            ))}
+            <div className="contract-name text-start">
+              <a
+                href={nftData.contractUrl}
+                className="h5 text-decoration-none"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {nftData.name}
+              </a>
+            </div>
+            <div>
+              {nftData.nfts.map((nft, i) => (
+                <NFT data={nft} contractName={nftData.name} key={i} />
+              ))}
+            </div>
           </div>
         ))}
       </div>
